92f4bf33283c1521e1295c8046d5b9fa4b276d61
Johan Niklasson <johan@nicklassons.se>
2017-11-05 16:07:46 +0100

    Added pascal source code

---
+++ src/pascal/fib.pas
@@ -0,0 +1,13 @@
+program fibonacci;
+
+function fib(n:integer): integer;
+begin
+    if (n <= 2) then
+        fib := 1
+    else
+        fib := fib(n-1) + fib(n-2);
+end;
+
+begin
+    write(fib(23));
+end.
