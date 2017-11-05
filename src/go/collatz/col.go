18766ee189c9fde267a81e0329234140cb470b81
Johan Niklasson <johan@nicklassons.se>
2017-11-05 20:42:06 +0100

    Added collatz go code

---
+++ src/go/collatz/col.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+    f := 0
+
+    for i := 1; i < 100000; i++ {
+        f = f + collatz(i)
+    }
+
+    fmt.Println(f)
+}
+
+func collatz(n int) int {
+	c := 0
+
+	for n > 1 {
+		if n % 2 == 0 {
+			n = n / 2
+		} else {
+			n = 3 * n + 1
+		}
+        c = c + 1
+	}
+
+	return c
+}
---
+++ src/go/collatz/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+go build -o col.o
+time ./col.o
