3086bbf0434b79692ca8bef7e317f23a72c56f4c
Johan Niklasson <johan@nicklassons.se>
2017-11-05 17:51:10 +0100

    Added code for c++

---
+++ src/c++/fib.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int);
+
+int main()
+{
+    int f = 0;
+    int i;
+
+    for (i = 0; i < 1; i++)
+    {
+        f = fibonacci(45);
+    }
+    cout << f << endl;
+
+    return 0;
+}
+
+int fibonacci(int n)
+{
+    if ( n <= 2 )
+    {
+        return 1;
+    }
+    else {
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+    }
+}
