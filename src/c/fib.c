eec93ade686c240f3e53c105b3de57fd0b39d5f9
Johan Niklasson <johan@nicklassons.se>
2017-11-05 17:33:19 +0100

    Added c code

---
+++ src/c/fib.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int fibonacci(int);
+
+int main()
+{
+    int f = 0;
+    int i;
+
+    for (i = 0; i < 1; i++)
+    {
+        f = fibonacci(45);
+    }
+    printf("%d\n", f);
+
+    return 0;
+}
+
+int fibonacci(int n)
+{
+    if ( n <= 2 )
+    {
+        return 1;
+    }
+    else {
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+    }
+}
